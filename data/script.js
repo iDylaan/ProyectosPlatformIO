4aab933e18312ce534f79bab4d32484f834d4fd3
Danii <108493547+iDylaan@users.noreply.github.com>
2023-04-13 12:23:58 -0600

    .

--- data/script.js
+++ data/script.js
@@ -1,49 +1,29 @@
-(() => {
+// document.querySelector('.bg-r').style.opacity = '0';
+// document.querySelector('.bg-g').style.opacity = '0';
 
-    let led_r = document.querySelector("#led_r");
-    let led_g = document.querySelector("#led_g");
-    let led_b = document.querySelector("#led_b");
-    const inputRanges = document.querySelectorAll("input[type='range']");
-    
-    setInterval(() => {
-        getPotes();
-    }, 2000);
-
-    inputRanges.forEach(range => {
-        range.addEventListener("click", async () => {
-            document.querySelector('.bg-r').style.opacity = led_r.value / 255;
-            document.querySelector('.bg-g').style.opacity = led_g.value / 255;
-            document.querySelector('.bg-b').style.opacity = led_b.value / 255;
-
-            leds = {
-                led1: led_r.value,
-                led2: led_g.value,
-                led3: led_b.value
-            }
-
-            await fetch("/led", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify(leds)
-            });
-        });
+document.querySelector('#frenos').addEventListener('click', async () => {
+    const response = await fetch("/reversa", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({})
     });
 
+    console.log(response);
 
-    async function getPotes() {
-        const response = await fetch("/potenciometros");
-        const data = await response.json();
-        document.querySelector(".pote-1").textContent = data.pote1;
-        document.querySelector(".pote-2").textContent = data.pote2;
-        document.querySelector(".pote-3").textContent = data.pote3;
-    }
+})
 
 
-    document.addEventListener("DOMContentLoaded", () => {
-        led_r.value = 0
-        led_g.value = 0
-        led_b.value = 0
+document.querySelector('#luces').addEventListener('click', async () => {
+    const response = await fetch("/luces", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({})
     });
-})();
+
+    console.log(response);
+
+})
--- src/main.cpp
+++ src/main.cpp
@@ -1,82 +1,61 @@
 #include "WiFi.h"
-#include "ESPAsyncWebServer.h"
-#include "AsyncTCP.h"
+#include "WiFiClient.h"
+#include "WebServer.h"
+#include "ESPmDNS.h"
 #include "SPIFFS.h"
-#include "ArduinoJson.h"
-#include "string"
+#include "string" 
 
 // Replace with your network credentials
-const char *ssid = "Dani's Network";
-const char *password = "kekojones1234";
+const char *ssid = "HUAWEI P20 lite 8357";
+const char *password = "12345678";
 
 // Set LED GPIO
-const int led_r = 4;
-const int led_g = 2;
-const int led_b = 5;
+const int ledTrasero1 = 18;
+const int ledTrasero2 = 4;
 
-// Set POTENCIOMETER GPIO
-const int pote_1 = 33;
-const int pote_2 = 32;
-const int pote_3 = 34;
+const int ledFrontarl1 = 2;
+const int ledFrontarl2 = 23;
+
+bool reversa = false;
+bool lucesEncendidad = false;
 
 // PWM
-const int ledCRed = 0;
-const int ledCGreen = 1;
-const int ledCBlue = 2;
-const int freq = 5000;
-const int res = 8;
+// const int ledCRed = 0;
+// const int ledCGreen = 1;
+// const int ledCBlue = 2;
+// const int freq = 5000;
+// const int res = 8;
 
 // Stores LED state
-String ledState;
+// String ledState;
 
 // Create AsyncWebServer object on port 80
-AsyncWebServer server(80);
+WebServer server(80);
 
-void handlePut(AsyncWebServerRequest *request) {
-  // Send OK response
-  request->send(200);
-}
 
-// Function to handle body of PUT request
-void handleLeds(AsyncWebServerRequest *request, uint8_t *data, size_t len, size_t index, size_t total)
-{
-  // Create a buffer to store the body data
-  char buffer[total + 1];
-  // Copy the data to the buffer and add a null terminator
-  memcpy(buffer, data, total);
-  buffer[total] = '\0';
-
-  // Create a JSON document with enough capacity to store the object
-  StaticJsonDocument<64> doc;
-
-  // Parse the JSON object from the buffer
-  DeserializationError error = deserializeJson(doc, buffer);
-
-  // Check for parsing errors
-  if (error)
-  {
-    Serial.print("deserializeJson() failed: ");
-    Serial.println(error.c_str());
-    return;
+// Funcion para manejar la reversa
+void handleReverse(){
+  if (reversa){
+    digitalWrite(ledTrasero1, HIGH);
+    digitalWrite(ledTrasero2, HIGH);
+  } else {
+    digitalWrite(ledTrasero1, LOW);
+    digitalWrite(ledTrasero2, LOW);
   }
 
-  // Get the values of led1, led2 and led3 from the JSON object
-  String led1 = doc["led1"];
-  String led2 = doc["led2"];
-  String led3 = doc["led3"];
+  reversa =! reversa;
+}
 
-  // Print the values for debugging purposes
-  Serial.print("Red: ");
-  Serial.println(led1);
-  Serial.print("Green: ");
-  Serial.println(led2);
-  Serial.print("Blue: ");
-  Serial.println(led3);
+void handleLuces(){
+  if (lucesEncendidad){
+    digitalWrite(ledFrontarl1, HIGH);
+    digitalWrite(ledFrontarl2, HIGH);
+  } else {
+    digitalWrite(ledFrontarl1, LOW);
+    digitalWrite(ledFrontarl2, LOW);
+  }
 
-  // Turn on or off the LEDs according to the values received
-  ledcWrite(ledCRed, led1.toInt());
-  ledcWrite(ledCGreen, led2.toInt());
-  ledcWrite(ledCBlue, led3.toInt());
+  lucesEncendidad =! lucesEncendidad;
 }
 
 void setup()
@@ -84,13 +63,10 @@ void setup()
   // Serial port for debugging purposes
   Serial.begin(115200);
 
-  ledcSetup(ledCRed, freq, res);
-  ledcSetup(ledCGreen, freq, res);
-  ledcSetup(ledCBlue, freq, res);
-
-  ledcAttachPin(led_r, ledCRed);
-  ledcAttachPin(led_g, ledCGreen);
-  ledcAttachPin(led_b, ledCBlue);
+  pinMode(ledTrasero1, OUTPUT);
+  pinMode(ledTrasero2, OUTPUT);
+  pinMode(ledFrontarl1, OUTPUT);
+  pinMode(ledFrontarl1, OUTPUT);
 
   // Initialize SPIFFS
   if (!SPIFFS.begin(true))
@@ -105,7 +81,8 @@ void setup()
   Serial.println("Conection successful");
 
   // Wait for connection
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print(".");
     Serial.println();
@@ -116,45 +93,21 @@ void setup()
   Serial.println(ssid);
   Serial.println(WiFi.localIP());
 
-  // Route for root / web page
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) { 
-    request->send(SPIFFS, "/index.html", "text/html"); 
-  });  
 
-  server.on("/styles.css", HTTP_GET, [](AsyncWebServerRequest *request) { 
-    request->send(SPIFFS, "/styles.css", "text/css"); 
-  });
+  server.serveStatic("/", SPIFFS, "/index.html");
+  server.serveStatic("/styles.css", SPIFFS, "/styles.css");
+  server.serveStatic("/script.js", SPIFFS, "/script.js");
 
-  server.on("/script.js", HTTP_GET, [](AsyncWebServerRequest *request) { 
-    request->send(SPIFFS, "/script.js", "application/javascript"); 
-  });
+  server.on("/reversa", handleReverse);
+  server.on("/luces", handleLuces);
 
-  // Route to set GPIO to HIGH
-  server.on("/led_r", HTTP_GET, [](AsyncWebServerRequest *request) {
-    if (request->hasParam("value")) {
-      String value = request->getParam("value")->value();
-      int brightness = value.toInt();
-      ledcWrite(ledCRed, brightness);
-    }
-
-    request->send(SPIFFS, "/index.html", String(), false); 
-  });
-
-  server.on("/led", HTTP_POST, handlePut, NULL, handleLeds);
-
-  // Route to get current time as JSON object
-  server.on("/potenciometros", HTTP_GET, [](AsyncWebServerRequest *request) {
-    AsyncResponseStream *response = request->beginResponseStream("application/json");
-    DynamicJsonDocument doc(1024);
-    doc["pote1"] = analogRead(pote_1);
-    doc["pote2"] = analogRead(pote_2);
-    doc["pote3"] = analogRead(pote_3);
-    serializeJson(doc, *response);
-    request->send(response); 
-  });
 
   // Start server
   server.begin();
 }
 
-void loop(){}
+void loop() {
+
+  server.handleClient();
+
+}
